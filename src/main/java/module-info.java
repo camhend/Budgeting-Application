73c20a8799b130828620fed61ffe2749f6b7474a
Sukhnain Deol <101545430+TheCowCompany@users.noreply.github.com>
2023-04-23 19:11:39 -0700

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.application.budgeter {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.application.budgeter to javafx.fxml;
-    exports com.application.budgeter;
-}
