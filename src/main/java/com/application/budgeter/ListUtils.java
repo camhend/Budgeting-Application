3116e0c00f13ac0115e595287ccea1912c663027
Cameron Henderson <cameronhenderson11@hotmail.com>
2023-06-01 21:13:46 -0700

    implement merge sort

--- src/main/java/com/application/budgeter/ListUtils.java
+++ src/main/java/com/application/budgeter/ListUtils.java
@@ -7,7 +7,7 @@ public class ListUtils {
     public static void sort (List<Expense> list, Comparator<Expense> c) {
         Expense[] array = list.toArray(new Expense[list.size()]);
         // check for empty or small list
-        quickSort(array, c, 0, array.length - 1);
+        mergeSort(array, c, 0, array.length - 1);
         ListIterator<Expense> itr = list.listIterator();
         for (int i=0; i < array.length; i++) {
             itr.next();
@@ -15,13 +15,70 @@ public class ListUtils {
         }
     }
 
+    // Recursively half the array into successively smaller subarrays.
+    // Sort each half, then merge the two halves together.
+    private static void mergeSort(Expense[] a, Comparator<Expense> c, int left, int right) {
+        if (left < right) {
+            int middle = (left + right) / 2;
+            mergeSort(a, c, left, middle);      // sort left subarray
+            mergeSort(a, c, middle + 1, right); // sort right subarray
+            merge(a, c, left, middle, right);   // merge the subarrays
+        }
+    }
+
+    // Interal method that takes an array and indeces for the left wall,
+    // middle point, and right wall. Walks through both subarrays, merging
+    // elements from both arrays into one into one sorted array.
+    private static void merge(Expense[] a, Comparator<Expense> c, int left, int middle, int right) {
+        
+        // Copy the the array into left and right subarrays
+        int leftSize = middle - left + 1;
+        int rightSize = right - middle;
+        Expense[] leftArray = new Expense[leftSize];
+        Expense[] rightArray = new Expense[rightSize];
+        for (int i = 0; i < leftSize; i++) {
+            leftArray[i] = a[left + i];
+        }
+        for (int j = 0; j < rightSize; j++) {
+            rightArray[j] = a[middle + 1 + j];
+        }
+
+        // Walk through both arrays, compare next element
+        // in both arrays, and put the smallest of the two
+        // into the final array.
+        int i = 0;
+        int j = 0;
+        int k = left;
+        while (i < leftSize && j < rightSize) {
+            if (c.compare(leftArray[i], rightArray[j]) <= 0) {
+                a[k] = leftArray[i];
+                i++;
+            } else {
+                a[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        // If any elements left in either array, then
+        // put all remaining into the final array.
+        while (i < leftSize) {
+             a[k] = leftArray[i];
+             i++;
+             k++;
+        }
+        while (j < rightSize) {
+            a[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
     private static void quickSort(Expense[] a, Comparator<Expense> c, int low, int high) {
         if (low >= high) {
             return;
         }
-        
         if (low < high) {
-            //int pivot =  high; // change this (just picks the last item in the array)
             int pivot = partition(a, c, low, high);
             quickSort(a, c, low, pivot - 1);
             quickSort(a, c, pivot + 1, high);
